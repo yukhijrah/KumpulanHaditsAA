e82004d32476984677c77e937ed4536f903a25c2
annisatahira <annisatahira999@gmail.com>
2018-10-22 19:51:50 +0700

    Tugas Kumpulan Hadits

---
+++ app/src/main/java/minamita/app/kumpulanhadits/DetailActivity.java
@@ -0,0 +1,35 @@
+package minamita.app.kumpulanhadits;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by annisatahira on 13/10/18.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView txtDetailJudul, txtDetailAyat, txtDetailArti;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        txtDetailJudul = (TextView) findViewById(R.id.txt_detail_judul);
+        txtDetailAyat = (TextView) findViewById(R.id.txt_detail_ayat);
+        txtDetailArti = (TextView) findViewById(R.id.txt_detail_arti);
+
+        getData();
+    }
+
+    private void getData() {
+        txtDetailJudul.setText(getIntent().getStringExtra("id_judul"));
+        txtDetailAyat.setText(getIntent().getStringExtra("id_ayat"));
+        txtDetailArti.setText(getIntent().getStringExtra("id_arti"));
+    }
+}
